ce7174f6e8dbb4c15e2c0225214b921fad84c7a2
Elisa Covato <elisa.covato@gmail.com>
2018-06-07 13:44:07 +0100

    Add first requirements

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 var catElem = document.querySelector(".cat");
-var catImage = document.querySelector(".cat-img");
+var catImage = document.querySelectorAll(".cat-img");
 var catCount = document.querySelector(".cat-count");
 
 var clickCounter = 0;
 
-catImage.addEventListener('click', function(){
+for(var i=0; i<catImage.length; i++) {
+	catImage[i].addEventListener('click', function(){
 	clickCounter++;
   catCount.innerHTML = clickCounter;
 }, false);
+};
+
